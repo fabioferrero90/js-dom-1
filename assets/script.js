dd52fa99634c7926fdadefce09d668388e510808
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-21 14:13:40 +0100

    feat: lamp have life + better ui

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,39 @@
-const lamp = document.getElementById('lamp')
-const toggleButton = document.getElementById('toggle')
-let lightsOff = true
+const lamp = document.getElementById('lamp');
+const toggleButton = document.getElementById('toggle');
+const alert = document.querySelector('.alert');
+const getLampLife = () => Math.ceil(Math.random() * (10 - 4) + 4);
+
+let lightsOff = true;
+let lampLife = getLampLife();
 
 toggleButton.addEventListener('click', () => {
-  lightsOff ? turnOnLamp() : turnOffLamp();
-  lightsOff = !lightsOff
+  if (toggleButton.innerHTML !== "Cambia la lampadina") {
+    lightsOff ? turnOnLamp() : turnOffLamp();
+    lightsOff = !lightsOff;
+  } else {
+    lampLife = getLampLife()
+    lamp.src = "./assets/img/white_lamp.png";
+    alert.classList.add('d_none');
+    toggleButton.innerHTML = "Accendi la luce";
+    lightsOff = true;
+  }
 })
 
 function turnOnLamp() {
-  lamp.src = "./assets/img/yellow_lamp.png";
-  lamp.classList.add('reverse')
-  toggleButton.innerHTML = "Spegni la luce"
+  if (lampLife > 0) {
+    lamp.src = "./assets/img/yellow_lamp.png";
+    lamp.classList.add('reverse');
+    toggleButton.innerHTML = "Spegni la luce";
+    lampLife--;
+  } else {
+    lamp.src = "./assets/img/broken-lamp.png";
+    alert.classList.remove('d_none');
+    toggleButton.innerHTML = "Cambia la lampadina";
+  }
 }
 
 function turnOffLamp() {
   lamp.src = "./assets/img/white_lamp.png";
-  lamp.classList.remove('reverse')
-  toggleButton.innerHTML = "Accendi la luce"
+  lamp.classList.remove('reverse');
+  toggleButton.innerHTML = "Accendi la luce";
 }
