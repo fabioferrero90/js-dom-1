072b7bbd015216ecd5528d150b096013e80f4b43
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-21 14:29:43 +0100

    feat: add page lightning

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 const lamp = document.getElementById('lamp');
 const toggleButton = document.getElementById('toggle');
 const alert = document.querySelector('.alert');
+const body = document.querySelector('body')
+const info = document.querySelector('.container > p')
+const container = document.querySelector('.container')
 const getLampLife = () => Math.ceil(Math.random() * (10 - 4) + 4);
 
 let lightsOff = true;
@@ -23,6 +26,7 @@ function turnOnLamp() {
   if (lampLife > 0) {
     lamp.src = "./assets/img/yellow_lamp.png";
     lamp.classList.add('reverse');
+    getLight()
     toggleButton.innerHTML = "Spegni la luce";
     lampLife--;
   } else {
@@ -35,5 +39,18 @@ function turnOnLamp() {
 function turnOffLamp() {
   lamp.src = "./assets/img/white_lamp.png";
   lamp.classList.remove('reverse');
+  getDark()
   toggleButton.innerHTML = "Accendi la luce";
+}
+
+function getDark() {
+  body.style.backgroundColor = "black";
+  info.style.color = "white";
+  container.style.borderColor = "white";
+}
+
+function getLight() {
+  body.style.backgroundColor = "white";
+  info.style.color = "black";
+  container.style.borderColor = "black";
 }
